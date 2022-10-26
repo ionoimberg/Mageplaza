23e65c04857acef604f23bb2ebce277ef6a315b0
Ion Oimberg <ionoimberg@gmail.com>
2022-10-26 13:27:47 +0300

    Add Product Attribute

--- app/code/Mageplaza/HelloWorld/Setup/InstallData.php
+++ app/code/Mageplaza/HelloWorld/Setup/InstallData.php
@@ -6,44 +6,43 @@ use Magento\Eav\Setup\EavSetupFactory;
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
-use Magento\Eav\Model\Config;
-use Magento\Customer\Model\Customer;
 
 class InstallData implements InstallDataInterface
 {
     private $eavSetupFactory;
 
-    public function __construct(EavSetupFactory $eavSetupFactory, Config $eavConfig)
+    public function __construct(EavSetupFactory $eavSetupFactory)
     {
         $this->eavSetupFactory = $eavSetupFactory;
-        $this->eavConfig       = $eavConfig;
     }
 
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
         $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
         $eavSetup->addAttribute(
-            \Magento\Customer\Model\Customer::ENTITY,
+            \Magento\Catalog\Model\Product::ENTITY,
             'sample_attribute',
             [
-                'type'         => 'varchar',
-                'label'        => 'Sample Attribute',
-                'input'        => 'text',
-                'required'     => false,
-                'visible'      => true,
-                'user_defined' => true,
-                'position'     => 999,
-                'system'       => 0,
+                'type' => 'text',
+                'backend' => '',
+                'frontend' => '',
+                'label' => 'Sample Attribute',
+                'input' => 'text',
+                'class' => '',
+                'source' => '',
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => true,
+                'user_defined' => false,
+                'default' => '',
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'used_in_product_listing' => true,
+                'unique' => false,
+                'apply_to' => ''
             ]
         );
-        $sampleAttribute = $this->eavConfig->getAttribute(Customer::ENTITY, 'sample_attribute');
-
-        // more used_in_forms ['adminhtml_checkout','adminhtml_customer','adminhtml_customer_address','customer_account_edit','customer_address_edit','customer_register_address']
-        $sampleAttribute->setData(
-            'used_in_forms',
-            ['adminhtml_customer']
-
-        );
-        $sampleAttribute->save();
     }
 }
