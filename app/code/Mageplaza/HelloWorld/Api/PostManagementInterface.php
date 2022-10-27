d3c7a2da3be2f5ebbe71ac815a10491917bbb1b5
Ion Oimberg <ionoimberg@gmail.com>
2022-10-27 16:16:32 +0300

    Modified ../PostManagementInterface.php

--- app/code/Mageplaza/HelloWorld/Api/PostManagementInterface.php
+++ app/code/Mageplaza/HelloWorld/Api/PostManagementInterface.php
@@ -1,15 +1,15 @@
 <?php
-namespace Mageplaza\HelloWorld\Api;
-
-
-interface PostManagementInterface {
-
-
-    /**
-     * GET for Post api
-     * @param string $param
-     * @return string
-     */
-
-    public function getPost($param);
-}
+//namespace Mageplaza\HelloWorld\Api;
+//
+//
+//interface PostManagementInterface {
+//
+//
+//    /**
+//     * GET for Post api
+//     * @param string $param
+//     * @return string
+//     */
+//
+//    public function getPost($param);
+//}
