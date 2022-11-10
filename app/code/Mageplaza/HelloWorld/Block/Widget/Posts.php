01f51c8dcfc70a68f27f45e3be895e3bc69b00da
Ion Oimberg <ionoimberg@gmail.com>
2022-11-10 15:17:39 +0200

    Create Widget

---
+++ app/code/Mageplaza/HelloWorld/Block/Widget/Posts.php
@@ -0,0 +1,12 @@
+<?php
+namespace Mageplaza\HelloWorld\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Posts extends Template implements BlockInterface
+{
+
+    protected $_template = "widget/posts.phtml";
+
+}
